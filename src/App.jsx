e72923db433e168c7ae0bc59366b460fc7e0ead5
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2023-11-04 20:55:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import style from './style/App.module.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <div id={style.logo}></div>
+      </header>
+    </div>
+  )
+}
+
+export default App
